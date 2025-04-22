4f47f6ec17bee54025f99d59595f8f6ef9852f9b
royfuwei <royfuwei@gmail.com>
2025-04-22 10:06:30 +0800

    feat: helm-install-latest.sh add namespace

--- .scripts/helm-install-latest.sh
+++ .scripts/helm-install-latest.sh
@@ -6,14 +6,21 @@ helm registry login \
   --password $HARBOR_TOKEN \
   $HARBOR_HOST
 
+echo "namespace: $NAMESPACE"
+
+if [ -z "$NAMESPACE" ]; then
+  echo "NAMESPACE is not set. Using default namespace."
+  NAMESPACE="default"
+fi
+
 
 RELEASE_NAME="$PROJECT_NAME"
 CHART_PATH="oci://$HARBOR_HOST/$PROJECT_SOURCE/$PROJECT_NAME"
 
 if helm status "$RELEASE_NAME" > /dev/null 2>&1; then
   echo "Release $RELEASE_NAME exists. Upgrading..."
-  helm upgrade "$RELEASE_NAME" "$CHART_PATH"
+  helm upgrade "$RELEASE_NAME" "$CHART_PATH --namespace $NAMESPACE --create-namespace"
 else
   echo "Release $RELEASE_NAME does not exist. Installing..."
-  helm install "$RELEASE_NAME" "$CHART_PATH"
+  helm install "$RELEASE_NAME" "$CHART_PATH --namespace $NAMESPACE --create-namespace"
 fi
